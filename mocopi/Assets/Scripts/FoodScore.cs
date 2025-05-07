e448d2a04581c2358054ef7f688129561f3e6968
fujita776 <g2227066@st.t-kougei.ac.jp>
2025-05-07 12:05:54 +0900

    Revert "Merge pull request #18 from rio123o/VegetableAndDonut"
    This reverts commit 72e6648e3dd88379ff6d466769640aba1f05a7fa, reversing
    changes made to 8d0cb4baa326a904c781feae063fa7c060293943.

--- mocopi/Assets/Scripts/FoodScore.cs
+++ mocopi/Assets/Scripts/FoodScore.cs
@@ -39,12 +39,6 @@ public class FoodScore : MonoBehaviour
                 break;
         }
 
-        FindObjectOfType<FoodScoreUI>()?.UpdateScoreText();
-
         Debug.Log($"[ScoreManager] Donut={donutCount}, Vegetable={vegetableCount}");
     }
-
-    public int GetDonutCount() => donutCount;
-    public int GetVegetableCount() => vegetableCount;
-
 }
